7ea2ef549ca40257a642ebd1e14249561bf4dc5e
Vanquisher <vanquisher@localhost.localdomain>
2019-06-18 09:11:28 +0530

    done almost

--- scanner.py
+++ scanner.py
@@ -15,14 +15,16 @@ nm_scan=nm_scanner.scan(sys.argv[1],'80',arguments='-O')
 host_is_up="The host is:"+ nm_scan['scan'][sys.argv[1]]['status']['state']+".\n"
 port_open="Port 80 is:"+ nm_scan['scan'][sys.argv[1]]['tcp'][80]['state']+".\n"
 method_scan="Scanning method is:"+nm_scan['scan'][sys.argv[1]]['tcp'][80]['reason']+".\n"
-#predicted_os="There is %s percent chance that the OS is %s" %(nm_scan['scan'][sys.argv[1]['osmatch'][0]['accuracy'],nm_scan['scan'][sys.argv[1]['osmatch'][0]['osclass'][0]['osfamily'])+".\n"
+predicted_os="Predicted Operating System is:"+nm_scan['scan'][sys.argv[1]]['osmatch'][0]['osclass'][0]['osfamily']+".\n"
+prediction_per="OS Prediction percentage is :"+nm_scan['scan'][sys.argv[1]]['osmatch'][0]['accuracy']+".\n"
 
 
-predicted_os="There is {} ".format("hello")
+
+#predicted_os="There is {} ".format("hello")
 
 
 with open("osinfo.txt",'w') as f:
-    f.write(host_is_up+port_open+method_scan+predicted_os)
-    f.write("\Timestamp:"+time.strftime("%Y-%m-%d_%H:%M:%S GMT",time.gmtime()))
+    f.write(host_is_up+port_open+method_scan+predicted_os+prediction_per)
+    f.write("\nTimestamp:"+time.strftime("%Y-%m-%d_%H:%M:%S GMT",time.gmtime()))
 
 print("\nCompleted....")
